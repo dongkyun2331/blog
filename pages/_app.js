53b1ef0da7f18564aeab0a5f5a371e0dfe99df19
포리 <dongkyun2331@gmail.com>
2024-06-15 10:29:24 +0000

    7

--- pages/_app.js
+++ pages/_app.js
@@ -3,31 +3,8 @@ import "../styles/globals.css";
 import "../components/Layout";
 import Layout from "../components/Layout";
 import { ThemeProvider } from "@/context/ThemeContext";
-import { useEffect } from 'react';
-import Router from 'next/router';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      fetch("/api/track", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ path: url }),
-      });
-    };
-
-    Router.events.on("routeChangeComplete", handleRouteChange);
-
-    // For the initial page load
-    handleRouteChange(window.location.pathname);
-
-    return () => {
-      Router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, []);
-
   return (
     <ThemeProvider>
       <Layout>
